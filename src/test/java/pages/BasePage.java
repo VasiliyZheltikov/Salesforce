95fb1c215f77839b16a6d69288b90e37dbf9efc9
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-08-04 13:45:44 +0300

    Fixed BasePage (added waiting)

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,12 +2,13 @@ package pages;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
     WebDriver driver;
     JavascriptExecutor js;
-
+    WebDriverWait wait;
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.js = (JavascriptExecutor) driver;
