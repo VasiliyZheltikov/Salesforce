caeea69ed92c347273c4b3e4e0931e31e92a0237
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-08-05 14:26:44 +0300

    Added Lombok, refactored Account and AccountTest with using Lombok plugin and builder

--- src/test/java/dto/Account.java
+++ src/test/java/dto/Account.java
@@ -1,106 +1,45 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class Account {
 
-    String name;
-    String phone;
-    String rating;
-    String fax;
-    String accountNumber;
-    String website;
-    String accountSite;
-    String tickerSymbol;
-    String type;
-    String ownership;
-    String industry;
-    String employees;
-    String annualRevenue;
-    String sicCode;
-    String billingStreet;
-    String shippingStreet;
+    @Builder.Default
+    String name = "Test";
+    @Builder.Default
+    String phone = null;
+    @Builder.Default
+    String rating = "Warm";
+    @Builder.Default
+    String fax = null;
+    @Builder.Default
+    String accountNumber = null;
+    @Builder.Default
+    String website = null;
+    @Builder.Default
+    String accountSite = null;
+    @Builder.Default
+    String tickerSymbol = null;
+    @Builder.Default
+    String type = "Customer - Direct";
+    @Builder.Default
+    String ownership = "Subsidiary";
+    @Builder.Default
+    String industry = "Media";
+    @Builder.Default
+    String employees = null;
+    @Builder.Default
+    String annualRevenue = null;
+    @Builder.Default
+    String sicCode = null;
+    @Builder.Default
+    String billingStreet = null;
+    @Builder.Default
+    String shippingStreet = null;
 
-    public Account(String name, String phone, String rating, String fax, String accountNumber, String website,
-                   String accountSite, String tickerSymbol, String type, String ownership, String industry,
-                   String employees, String annualRevenue, String sicCode, String billingStreet, String shippingStreet) {
-        this.name = name;
-        this.phone = phone;
-        this.rating = rating;
-        this.fax = fax;
-        this.accountNumber = accountNumber;
-        this.website = website;
-        this.accountSite = accountSite;
-        this.tickerSymbol = tickerSymbol;
-        this.type = type;
-        this.ownership = ownership;
-        this.industry = industry;
-        this.employees = employees;
-        this.annualRevenue = annualRevenue;
-        this.sicCode = sicCode;
-        this.billingStreet = billingStreet;
-        this.shippingStreet = shippingStreet;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getRating() {
-        return rating;
-    }
-
-    public String getFax() {
-        return fax;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public String getAccountSite() {
-        return accountSite;
-    }
-
-    public String getTickerSymbol() {
-        return tickerSymbol;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getOwnership() {
-        return ownership;
-    }
-
-    public String getIndustry() {
-        return industry;
-    }
-
-    public String getEmployees() {
-        return employees;
-    }
-
-    public String getAnnualRevenue() {
-        return annualRevenue;
-    }
-
-    public String getSicCode() {
-        return sicCode;
-    }
-
-    public String getBillingStreet() {
-        return billingStreet;
-    }
-
-    public String getShippingStreet() {
-        return shippingStreet;
-    }
 }
--- src/test/java/tests/AccountTest.java
+++ src/test/java/tests/AccountTest.java
@@ -1,21 +1,33 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import dto.Account;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static dto.AccountFactory.getAccount;
-
 
 public class AccountTest extends BaseTest {
 
     @Test
     public void checkCreateAccount(){
-        Account account = getAccount(
-                "Warm",
-                "Customer - Direct",
-                "Subsidiary",
-                "Media");
+        Faker faker = new Faker();
+        Account account = Account.builder()
+                .name(faker.company().name())
+                .phone(faker.phoneNumber().phoneNumber())
+                .rating("Hot")
+                .fax(faker.phoneNumber().phoneNumber())
+                .accountNumber(faker.number().digits(5))
+                .website(faker.internet().domainName())
+                .accountSite(faker.internet().domainName())
+                .tickerSymbol(faker.stock().nsdqSymbol())
+                .type("Customer - Direct")
+                .ownership("Subsidiary")
+                .industry("Media")
+                .employees(faker.number().digits(2))
+                .annualRevenue(faker.number().digits(7))
+                .sicCode(faker.number().digits(5))
+                .billingStreet(faker.address().streetAddress())
+                .shippingStreet(faker.address().streetAddress())
+                .build();
         loginPage.open()
                 .login("tborodich@tms.sandbox", "Password003!")
                 .open();
